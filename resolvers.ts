6b6e91d865522aec652bd9ed160d929a34d2b603
Tri Ngoc <caotringoc11@gmail.com>
2025-02-13 23:33:38 +0700

    CRUD: read a article, create a article

--- resolvers.ts
+++ resolvers.ts
@@ -8,6 +8,23 @@ export const resolvers = {
         getListArticle: async () => {
             const articles = await Article.find({ deleted: false });
             return articles; 
+        },
+        getArticle: async (_: any, args: any) => {
+            const { id } = args;
+            const article = await Article.findOne({ 
+                _id: id,
+                deleted: false
+            });
+            return article; 
+        }
+    },
+    Mutation: {
+        createArticle: async (_: any, args: any) => {
+            const { article } = args;
+            const record = new Article(article);
+            await record.save();
+            
+            return record;
         }
     }
 }
