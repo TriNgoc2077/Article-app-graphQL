e1e6e1eaf0b575162a88155d83e97dd5cf2bb6d3
Tri Ngoc <caotringoc11@gmail.com>
2025-02-13 23:08:41 +0700

    CRUD: Read articles

--- resolvers.ts
+++ resolvers.ts
@@ -1,7 +1,13 @@
+import Article from "./Model/article.Model";
+
 export const resolvers = {
     Query: {
         hello: () => {
             return 'hello world';
+        },
+        getListArticle: async () => {
+            const articles = await Article.find({ deleted: false });
+            return articles; 
         }
     }
 }
